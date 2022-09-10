788c2b0e2ef4d01f3251f701187d312be773bcd8
Nilda Maria Vasquez Rodriguez (24038) <nilda.vasquez@est.zamorano.edu>
2022-09-09 22:45:44 -0600

    Ejercicio 8.3

---
+++ labs/lab02/celsius2farenheit.py
@@ -0,0 +1,5 @@
+# Tendencias e Innovacion en Tecnologias Agricola (TEA)
+
+celsius = input("Ingrese temperatura en grados Celsius: ")
+farenheit = (float(celsius)) * 9/5 + 32
+print("La temperatura en grados Farenheit es: ", farenheit)
