cc7ed94050ef94527d0b7f5da47d697094d8bc45
Nilda Maria Vasquez Rodriguez (24038) <nilda.vasquez@est.zamorano.edu>
2022-09-28 19:42:44 -0600

    Lab 04

--- labs/lab04/exercise_4.7.py
+++ labs/lab04/exercise_4.7.py
@@ -1,13 +1,20 @@
-while True:
-    vegetal = input("Ingrese el tipo del vegetal: ")
-    if (vegetal == "salir"):
-        break
+# Tendencias e Innovación en Tecnologías Agrícolas (TEA)
+def computegrade(grade):
+    if (grade >= 0.9 and grade <= 1.0):
+        grade = "Sobresaliente"
+    elif (grade >= 0.8 and grade < 0.9):
+        grade = "Notable"
+    elif (grade >= 0.7 and grade < 0.8):
+        grade = "Bien"
+    elif (grade >= 0.6 and grade < 0.7):
+        grade = "Suficiente"
     else:
-        if (vegetal == "hortaliza"):
-            print("Ejemplo: Maíz")
-        elif (vegetal == "frutal"):
-            print("Ejemplo: Mango")
-        elif (vegetal == "ornamental"):
-            print("Ejemplo: Rosal")
-        else:
-            print("No definido")
+        grade = "Error, calificación no válida"
+    return grade 
+
+try:
+    score = float(input("Ingrese la calificación (0.01 - 1.00): "))
+    grade = computegrade(score)
+    print("El grado de su calificación es: ", grade)
+except:
+    print("Error, calificación no válida.")
